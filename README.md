3713237041e908f952e3ebc22356783b6cf5d181
root <root@ITCLPA70110.ditoadmin.crifnet.com>
2024-08-20 16:37:23 +0200

    init

--- README.md
+++ README.md
@@ -1,33 +1,58 @@
 # k8s-swiss-knife
+
 k8s-swiss-knife is the k8s plugin you ever wanted.
 
 <img src=asset/k8s-swiss-knife-removebg.png width=300>
 
+# Install
+
+```
+LATEST_TAG=$(curl https://api.github.com/repos/texano00/k8s-swiss-knife/tags | jq -r '.[0].name') && wget -O kubectl-swissknife "https://github.com/texano00/k8s-swiss-knife/releases/download/$LATEST_TAG/kubectl-swissknife" && sudo install kubectl-swissknife /usr/bin
+```
+
 # Usage
+
 ## help
-`kubectl swissknife -h `
+
+```
+kubectl swissknife -h
+```
 <img src=asset/helper.png>
 
-`kubectl swissknife root_less_checker -h`
+```
+kubectl swissknife root_less_checker -h
+```
 <img src=asset/root_less_checker_helper.png>
 
 ## root_less_checker
+
 ```
 kubectl swissknife root_less_checker [-n <namespace>]
 ```
 <img src=asset/root_less_checker.png>
 
 ## healthcheck
+
 ```
 kubectl swissknife healthcheck [-n <namespace>]
 ```
+<img src=asset/oom_checker.png>
 
-# Install
+## get_resource
+
 ```
-LATEST_TAG=$(curl https://api.github.com/repos/texano00/k8s-swiss-knife/tags | jq -r '.[0].name') && wget -O kubectl-swissknife "https://github.com/texano00/k8s-swiss-knife/releases/download/$LATEST_TAG/kubectl-swissknife" && sudo install kubectl-swissknife /usr/bin
+kubectl swissknife get_resource [-n <namespace>]
+```
+<img src=asset/get_resource.png>
+
+# Note
+Register binary with your shell’s completion framework by running `register-python-argcomplete`:
+```
+eval "$(register-python-argcomplete kubectl-swissknife)"
 ```
 
 # Development
+
 ```
 #! /bin/bash
 
@@ -40,3 +65,11 @@ pyinstaller -F --path venv/lib64/python3.10/site-packages kubectl-swissknife.py
 
 sudo cp dist/kubectl-swissknife /usr/local/bin/kubectl-swissknife
 ```
+
+# License
+
+This project is licensed under the Apache License 2.0 - see the [LICENSE](LICENSE) file for details.
+
+# Version
+
+Current version: 0.5.0 - see the [CHANGELOG](CHANGELOG) file for details.
--- plugin/utils/kubernetes.py
+++ plugin/utils/kubernetes.py
@@ -1,16 +1,21 @@
 """Kubernetes module"""
+
 import datetime
 import logging
 from kubernetes.client.rest import ApiException
 from kubernetes import client, config
 from kubernetes.stream import stream
 
+
 class Kubernetes:
     """Kubernetes class"""
 
     def __init__(self):
-        auth_strategy_mapper = {"incluster": config.load_incluster_config, "kubeconfig": config.load_kube_config}
-        auth_strategy_mapper['kubeconfig']()
+        auth_strategy_mapper = {
+            "incluster": config.load_incluster_config,
+            "kubeconfig": config.load_kube_config,
+        }
+        auth_strategy_mapper["kubeconfig"]()
         self.v1_apps = client.AppsV1Api()
         self.v1_core = client.CoreV1Api()
 
@@ -29,39 +34,54 @@ class Kubernetes:
         logging.info("Restarting %s in ns %s", deployment, namespace)
         now = datetime.datetime.utcnow()
         now = str(now.isoformat("T") + "Z")
-        body = {"spec": {"template": {"metadata": {"annotations": {"kubectl.kubernetes.io/restartedAt": now}}}}}
+        body = {
+            "spec": {
+                "template": {
+                    "metadata": {
+                        "annotations": {"kubectl.kubernetes.io/restartedAt": now}
+                    }
+                }
+            }
+        }
         try:
-            self.v1_apps.patch_namespaced_deployment(deployment, namespace, body, pretty="true")
+            self.v1_apps.patch_namespaced_deployment(
+                deployment, namespace, body, pretty="true"
+            )
         except ApiException as exception:
-            logging.error("Exception when calling AppsV1Api->read_namespaced_deployment_status: %s\n", exception)
+            logging.error(
+                "Exception when calling AppsV1Api->read_namespaced_deployment_status: %s\n",
+                exception,
+            )
         self.v1_apps = client.AppsV1Api()
         self.v1_core = client.CoreV1Api()
 
-    def get_pods(self, namespace, field_selector=''):
+    def get_pods(self, namespace, field_selector=""):
         """get_pods"""
-        pods = self.v1_core.list_namespaced_pod(namespace=namespace, field_selector=field_selector)
+        pods = self.v1_core.list_namespaced_pod(
+            namespace=namespace, field_selector=field_selector
+        )
         return pods
-    
 
     def exec_commands(self, namespace, pod_name, container_name, command):
         # Calling exec and waiting for response
-        exec_command = [
-            '/bin/sh',
-            '-c',
-            command]
+        exec_command = ["/bin/sh", "-c", command]
         # When calling a pod with multiple containers running the target container
         # has to be specified with a keyword argument container=<name>.
         err = False
         resp = ""
         try:
-            resp = stream(self.v1_core.connect_get_namespaced_pod_exec,
-                        pod_name,
-                        namespace,
-                        container=container_name,
-                        command=exec_command,
-                        stderr=True, stdin=False,
-                        stdout=True, tty=False)
-            if('not found' in resp or '' == resp):
+            resp = stream(
+                self.v1_core.connect_get_namespaced_pod_exec,
+                pod_name,
+                namespace,
+                container=container_name,
+                command=exec_command,
+                stderr=True,
+                stdin=False,
+                stdout=True,
+                tty=False,
+            )
+            if "not found" in resp or "" == resp:
                 raise Exception
         except Exception as e:
             err = True
